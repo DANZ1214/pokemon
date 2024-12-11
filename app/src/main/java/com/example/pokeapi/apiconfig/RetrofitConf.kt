02be646684c377d22d678563b651697c630cf3e4
WilsonFerrer <ferrerawilson76@gmail.com>
2024-12-11 01:11:08 -0600

    cambios

--- app/src/main/java/com/example/pokeapi/apiconfig/RetrofitConf.kt
+++ app/src/main/java/com/example/pokeapi/apiconfig/RetrofitConf.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "https://pokeapi.co/api/v2/" // Reemplaza con tu URL base
+    private const val BASE_URL = "https://pokeapi.co/api/v2/"
 
     val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
